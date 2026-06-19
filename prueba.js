a89297bd25a835f64662ba35f957e49bc6123cfc
AXELLEDGM <118194828+AXELLEDGM@users.noreply.github.com>
2026-06-18 22:52:41 -0600

    Rediseño visual y carrusel en productos
    Rediseño página principal con nuevo header y hero
    Agrego carrusel de fotos en tarjetas de productos
    Actualizo estilos y grid del catálogo

---
+++ prueba.js
@@ -0,0 +1,14 @@
+//Anagram
+
+'ana'
+function areAnagrams(word1, word2)
+{
+    let removeSpacesAndToLower = function (str)
+    {
+        let result = '';
+        for (const iterator of str)
+        {
+            
+        }
+    }
+}
